559a922bc956e4cbcd6467a856faa4eafa2a9611
Thomas Monk <t.d.monk@lse.ac.uk>
2026-05-22 18:44:32 +0100

    add data buffer discovery script

---
+++ scripts/find_data_buffer.py
@@ -0,0 +1,118 @@
+"""Find Stata data buffer pointer on Windows using sentinel values."""
+import ctypes
+import struct
+
+dll_path = r'C:\Program Files\StataNow19\se-64.dll'
+dll = ctypes.WinDLL(dll_path)
+handle = dll._handle
+
+# Init Stata
+dll.StataSO_Main.argtypes = [ctypes.c_int, ctypes.POINTER(ctypes.c_char_p)]
+dll.StataSO_Main.restype = ctypes.c_int
+dll.StataSO_Main(2, (ctypes.c_char_p * 2)(b'stata', b'-q'))
+
+dll.StataSO_Execute.argtypes = [ctypes.c_char_p]
+dll.StataSO_Execute.restype = ctypes.c_int
+
+# Load dataset
+dll.StataSO_Execute(b'sysuse auto, clear')
+
+# Find .data section
+with open(dll_path, 'rb') as f:
+    pe_data = f.read()
+e_lfanew = struct.unpack('<I', pe_data[0x3c:0x40])[0]
+opt_hdr_size = struct.unpack('<H', pe_data[e_lfanew+20:e_lfanew+22])[0]
+sh_off = e_lfanew + 24 + opt_hdr_size
+data_rva = 0
+data_size = 0
+for i in range(struct.unpack('<H', pe_data[e_lfanew+6:e_lfanew+8])[0]):
+    sh = pe_data[sh_off+i*40:sh_off+i*40+40]
+    name = sh[:8].rstrip(b'\x00').decode('utf-8', errors='replace')
+    if name == '.data':
+        data_rva = struct.unpack('<I', sh[12:16])[0]
+        data_vsize = struct.unpack('<I', sh[8:12])[0]
+        data_rawsize = struct.unpack('<I', sh[16:20])[0]
+        data_size = min(data_vsize, data_rawsize)
+        break
+
+data_ptr = handle + data_rva
+print('Data section: %x size %d' % (data_ptr, data_size))
+
+# Step 1: Create a sentinel variable with unique value
+import math
+sentinel_val = 12345.6789
+sentinel_bytes = struct.pack('<d', sentinel_val)
+print('Sentinel IEEE754 bytes:', sentinel_bytes.hex())
+
+# Create the variable
+dll.StataSO_Execute(b'gen double __px_sentinel = ' + str(sentinel_val).encode())
+print('Created __px_sentinel')
+
+# Step 2: The sentinel value is now stored in Stata's data buffer (heap memory)
+# Search all process memory for this value
+# First, let's check the .data section for pointers to the sentinel
+data_buf = (ctypes.c_char * data_size)()
+ctypes.memmove(data_buf, ctypes.c_void_p(data_ptr), data_size)
+raw = bytes(data_buf)
+
+# Read nvar to know which variable is our sentinel
+nv_buf = (ctypes.c_int * 1)()
+ctypes.memmove(nv_buf, ctypes.c_void_p(data_ptr + 0x211644), 4)
+nvar = nv_buf[0]
+print('Current nvar:', nvar)
+
+# __px_sentinel is the last variable (index = nvar)
+# We need to find the data buffer address
+# The data buffer stores all values in a contiguous block
+
+# Strategy: search .data section for pointers pointing to heap memory
+# that contains our sentinel value
+sentinel_match = None
+
+# Scan for 8-byte pointers in .data
+count = 0
+for off in range(0, data_size - 8, 8):
+    ptr = struct.unpack('<Q', raw[off:off+8])[0]
+    # Check if pointer looks like a heap address (not in DLL range)
+    if ptr < 0x10000 or (handle <= ptr < handle + 0x04000000):
+        continue
+    if ptr > 0x800000000000:
+        continue  # Not a valid user-space address
+    # Try to read a double at this address
+    try:
+        test_buf = (ctypes.c_double * 1)()
+        ctypes.memmove(test_buf, ctypes.c_void_p(ptr), 8)
+        if abs(test_buf[0] - sentinel_val) < 0.0001:
+            print('Found sentinel via pointer at .data+%x: ptr=%x val=%f' 
+                  % (off, ptr, test_buf[0]))
+            sentinel_match = (off, ptr)
+            count += 1
+    except:
+        pass
+
+print('Sentinel matches found: %d' % count)
+
+if sentinel_match:
+    off, data_buffer = sentinel_match
+    print('Data buffer pointer at .data+%x = %x' % (off, data_buffer))
+    
+    # The data buffer stores variables in order
+    # Each observation has a fixed stride
+    # Price is variable 1 (0-indexed) in auto dataset
+    # Read price for observation 0
+    
+    # Read price at obs 0 (value should be 4099)
+    price_buf = (ctypes.c_double * 1)()
+    # The data layout: each obs has nvar double values
+    # price is at position 0 in auto (variable index 0)
+    obs_size = nvar * 8  # each double is 8 bytes
+    price_offset = 0  # first variable = price
+    ctypes.memmove(price_buf, ctypes.c_void_p(data_buffer + price_offset * 8), 8)
+    print('Price[0] (expected 4099):', price_buf[0])
+    
+    # Also read mpg at obs 0 (variable index 1)
+    mpg_buf = (ctypes.c_double * 1)()
+    ctypes.memmove(mpg_buf, ctypes.c_void_p(data_buffer + 1 * 8), 8)
+    print('MPG[0] (expected 22):', mpg_buf[0])
+
+print('Done')
